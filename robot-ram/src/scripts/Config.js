3361d887479de50538eb374127862b3f41a687d7
han4n <163819@unsaac.edu.pe>
2022-07-17 12:35:07 -0500

    Tutorial done

--- robot-ram/src/scripts/Config.js
+++ robot-ram/src/scripts/Config.js
@@ -2,9 +2,6 @@ const Config ={
     ROSBRIDGE_SERVER_IP:"192.168.1.3",
     ROSBRIDGE_SERVER_PORT:"9090",
     TIME_RECONNECTION: 3000, //ms
-    //CMD_VEL_TOPIC:"/husky_velocity_controller/cmd_vel",
-    //POSE_TOPIC: "/amcl_pose ",
-    //ODOM_TOPIC: "/husky_velocity_controller/odom",
     CMD_VEL_TOPIC:"/cmd_vel",
     POSE_TOPIC: "/amcl_pose",
     ODOM_TOPIC: "/odom",
